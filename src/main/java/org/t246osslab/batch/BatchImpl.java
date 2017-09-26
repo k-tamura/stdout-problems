015a4ee6416ff6c440799414226f71caac385551
Kohei <ktamura.biz.80@gmail.com>
2017-09-26 23:03:38 +0900

    Remove code for Linux

--- src/main/java/org/t246osslab/batch/BatchImpl.java
+++ src/main/java/org/t246osslab/batch/BatchImpl.java
@@ -6,8 +6,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
@@ -16,59 +14,29 @@ import org.springframework.core.io.Resource;
  */
 public class BatchImpl implements BatchInterface {
 
-    protected Logger log = LoggerFactory.getLogger(this.getClass());
-
     public static void main(String[] args) {
         BatchImpl pi = new BatchImpl();
         int executeBatch = pi.executeBatch();
-        System.out.println(executeBatch);
+        System.out.println("Result: " + executeBatch);
     }
 
     @Override
     public int executeBatch() {
-        /* get a batch file path */
-        String batchFilePath = getBatchFilePath();
-
-        if (batchFilePath == null) {
-            log.error("Can't create a batch file");
-        } else {
-            try {
-                /* execute the batch */
+        String batchFilePath = null;
+        try {
+            Resource resource = new ClassPathResource("test.bat");
+            File batFile = resource.getFile();
+            if (!batFile.setExecutable(true)) {
+                batchFilePath = batFile.getAbsolutePath();
                 ProcessBuilder pb = new ProcessBuilder(batchFilePath);
                 Process process = pb.start();
                 printInputStream(process.getInputStream());
-                return process.hashCode();
-            } catch (IOException e) {
-                log.error("IOException occurs: ", e);
-            }
-        }
-        return -1;
-    }
-    
-    private String getBatchFilePath() {
-        String batchFilePath = null;
-        String osName = System.getProperty("os.name").toLowerCase();
-        String batFileName = null;
-        if (osName.toLowerCase().startsWith("windows")) {
-            batFileName = "test.bat";
-        } else {
-            batFileName = "test.sh";
-        }
-        Resource resource = new ClassPathResource("/bat/" + batFileName);
-        try {
-            File batFile = resource.getFile();
-            if (!batFile.setExecutable(true)) {
-                log.debug("batFile.setExecutable(true) returns false.");
-            }
-            batchFilePath = batFile.getAbsolutePath();
-            if (!osName.toLowerCase().startsWith("windows")) {
-                Runtime runtime = Runtime.getRuntime();
-                runtime.exec("chmod 777 " + batchFilePath);
+                return 0;
             }
         } catch (IOException e) {
-            log.error("IOException occurs: ", e);
+            e.printStackTrace();
         }
-        return batchFilePath;
+        return -1;
     }
 
     private void printInputStream(InputStream is) throws IOException {
